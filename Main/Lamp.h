7be5db62dbd713a7327ec24473dea0597383d28d
Albert-Tr-pp <tromsaalbert@gmail.com>
2025-10-23 14:45:31 +0300

    (Code rewriting - .h and .cpp merged. Extra methods added)

--- Main/Lamp.h
+++ Main/Lamp.h
@@ -4,26 +4,95 @@
 
 #include "Libraries.h"
 
+#define NUM_LEDS 64
+#define M_WIDTH 8
+#define M_HEIGHT 8
+
+template<uint8_t M_PIN>
 class Lamp {
   private:
     uint8_t prPin;
     uint8_t msPin;
+    microLED<NUM_LEDS, M_PIN, MLED_NO_CLOCK, LED_WS2812, ORDER_GRB, CLI_AVER> matrix;
 
   public:
-    Lamp(uint8_t prPin, uint8_t msPin);
+    Lamp(uint8_t prPin, uint8_t msPin)
+    : matrix(M_WIDTH, M_HEIGHT, PARALLEL, LEFT_TOP, DIR_RIGHT)
+    {
+      setPrPin(prPin);
+      setMsPin(msPin);
+    }
 
-    uint16_t getPrData();
-    void printPrData(uint16_t prData);
+  // -----------------------------------------------------------------------
 
-    uint16_t getMsData();
-    void printMsData(uint16_t msData);
+  uint16_t getPrData(){
+    uint16_t sum = 0;
+    for (int i = 0; i < 10; i++) {
+      sum += analogRead(prPin);
+    }
+    return sum / 10;
+  }
 
-//setters & getters
-    void setPrPin(uint8_t prPin);
-    void setMsPin(uint8_t msPin);
+  void printPrData(uint16_t prData){
+    Serial.print("ФОТОРЕЗИСТОР: ");
+    Serial.println(prData);
+  }
 
-    uint8_t getPrPin();
-    uint8_t getMsPin();
+  // -----------------------------------------------------------------------
+
+  uint16_t getMsData(){
+    return digitalRead(msPin);
+  }
+
+  void printMsData(uint16_t msData){
+    Serial.print("ДВИЖЕНИЕ: ");
+    Serial.println(msData);
+  }
+
+  // -----------------------------------------------------------------------
+
+  void testMatrix(){
+    //тест матрицы
+  }
+
+  void turnOnMatrix(){
+    //вкл матрицу
+  }
+
+  void turnOffMatrix(){
+    //выкл матрицу
+  }
+
+  // -----------------------------------------------------------------------
+
+
+  // -----------------------------------------------------------------------
+
+
+  // -----------------------------------------------------------------------
+
+
+  // -----------------------------------------------------------------------
+
+  //setters
+  void setPrPin(uint8_t prPin){
+    this->prPin = prPin;
+  }
+  void setMsPin(uint8_t msPin){
+    this->msPin = msPin;
+  }
+
+  //getters
+  uint8_t getPrPin(){
+    return prPin;
+  }
+  uint8_t getMsPin(){
+    return msPin;
+  }
+  uint8_t getMatPin(){
+    return M_PIN;
+  }
+
 };
 
 #endif
--- Main/Main.ino
+++ Main/Main.ino
@@ -4,19 +4,20 @@
 
 #define PR_PIN A3
 #define MS_PIN 4
+#define M_PIN 2
 
-Lamp lamp(PR_PIN, MS_PIN); 
+Lamp<M_PIN> lamp(PR_PIN, MS_PIN); 
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  // lamp.printPrData(lamp.getPrData());
-
-  // delay(100);
-
-  lamp.printMsData(lamp.getMsData());
+  lamp.printPrData(lamp.getPrData());
 
   delay(100);
+
+  // lamp.printMsData(lamp.getMsData());
+
+  // delay(100);
 }
